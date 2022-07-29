bdbdd0d15fcca3e46bbf0d758d401cdcf416f4f8
손기연 <sonky740@minwise.co.kr>
2022-07-29 16:53:48 +0900

    103~~

--- 86-data-fetch/pages/index.jsx
+++ 86-data-fetch/pages/index.jsx
@@ -1,11 +1,43 @@
-function HomePage(props) {
+import Link from 'next/link';
+import path from 'path';
+import fs from 'fs/promises';
+
+function HomePage({ products }) {
   return (
     <ul>
-      <li>Product 1</li>
-      <li>Product 2</li>
-      <li>Product 3</li>
+      {products.map((product) => (
+        <li key={product.id}>
+          <Link href={`/${product.id}`}>{product.title}</Link>
+        </li>
+      ))}
     </ul>
   );
+}
+
+export async function getStaticProps(context) {
+  console.log('(RE-)Generating...');
+  const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData);
+
+  if (!data) {
+    return {
+      redirect: {
+        destination: '/no-data',
+      },
+    };
+  }
+
+  if (data.products.length === 0) {
+    return { notFound: true };
+  }
+
+  return {
+    props: {
+      products: data.products,
+    },
+    revalidate: 10,
+  };
 }
 
 export default HomePage;
