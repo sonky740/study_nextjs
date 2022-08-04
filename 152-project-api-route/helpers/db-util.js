8abcfb0777092f7971e2b76c9e280ea9778ad8cc
손기연 <sonky740@gmail.com>
2022-08-04 14:48:08 +0900

    166~

---
+++ 152-project-api-route/helpers/db-util.js
@@ -0,0 +1,27 @@
+import { MongoClient } from 'mongodb';
+
+export async function connectDatabase() {
+  const client = await MongoClient.connect(
+    'mongodb+srv://sonky740_nextjs:eqwwr@cluster0.reatrgr.mongodb.net/events?retryWrites=true&w=majority'
+  );
+  return client;
+}
+
+export async function insertDocument(client, collection, document) {
+  const db = client.db();
+
+  const result = await db.collection(collection).insertOne(document);
+  return result;
+}
+
+export async function getAllDocuments(client, collection, sort) {
+  const db = client.db();
+
+  const documents = await db
+    .collection('comments')
+    .find()
+    .sort({ _id: -1 }) // comments를 id 기준으로 내림차순으로 정렬
+    .toArray();
+
+  return documents;
+}
